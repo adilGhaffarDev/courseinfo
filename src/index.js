69731d41c1c23dee62c9b60640f114dc30c3d7d6
adil ghaffar <adilghaffar64@gmail.com>
2020-07-28 04:26:34 +0300

    course info part 1 and 2 done

--- src/index.js
+++ src/index.js
@@ -4,27 +4,37 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => {
   return (
-    <div>
+    <>
       <h1>{props.course}</h1>
-    </div>
+    </>
   )
 }
 
 const Content = (props) => {
   return (
-    <div>
+    <>
+      <Part name = {props.p1} exercise = {props.ex1}/>
+      <Part name = {props.p2} exercise = {props.ex2}/>
+      <Part name = {props.p3} exercise = {props.ex3}/>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
       <p>
-        {props.part} {props.exercises}
+        {props.name} {props.exercise}
       </p>
-    </div>
+    </>
   )
 }
 
 const Total = (props) => {
   return (
-    <div>
+    <>
      <p>Number of exercises {props.allexercices}</p>
-    </div>
+    </>
   )
 }
 
@@ -38,13 +48,11 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
+    <>
       <Header course={course}/>
-      <Content part={part1} exercises = {exercises1}/>
-      <Content part={part2} exercises = {exercises2}/>
-      <Content part={part3} exercises = {exercises3}/>
+      <Content p1={part1} ex1 = {exercises1} p2={part2} ex2 = {exercises2} p3={part3} ex3 = {exercises3}/>
       <Total allexercices = {exercises1 + exercises2 + exercises3}/>
-    </div>
+    </>
   )
 }
 
